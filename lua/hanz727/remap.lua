23bdc475ae4fa8a18096eb2274853e6b2aecd2f1
Oskar Rasienski <oskar.rasienski@gmail.com>
2025-02-10 13:24:52 +0100

    remap for C+w

--- lua/hanz727/remap.lua
+++ lua/hanz727/remap.lua
@@ -19,3 +19,6 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set('n', '<C-w>', '<C-\\><C-n><C-w>')
+vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>')
